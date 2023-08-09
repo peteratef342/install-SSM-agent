7b7e69cfbc5f4989e13f11aee4e8b8571c43a853
peteratef342 <peteratef342@users.noreply.github.com>
2023-08-09 11:59:37 +0200

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -4,5 +4,4 @@ maintainer_email 'jonathan@serafini.ca'
 license 'Apache 2.0'
 description 'Amazon EC2 Simple Systems Manager (SSM) agent'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-source_url 'https://github.com/JonathanSerafini/chef-ssm_agent'
 version '0.1.0'
